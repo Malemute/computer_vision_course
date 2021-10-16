07f74b353b1f7217fb0b95909d9f5ce07d3d5868
Malemute <eugeny.mamontov@gmail.com>
2021-10-16 19:33:31 +0300

    initial

---
+++ HandTrackingModule.py
@@ -0,0 +1,73 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class HandDetector:
+    def __init__(self,
+                 mode=False,
+                 max_hands=2,
+                 detection_con=0.5,
+                 track_con=0.5
+                 ):
+        self.mode = mode
+        self.maxHands = max_hands
+        self.detectionCon = detection_con
+        self.trackCon = track_con
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def find_hands(self, img, draw=True):
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(img_rgb)
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def find_position(self, img, hand_no=0, draw=True):
+        lm_list = []
+        if self.results.multi_hand_landmarks:
+            my_hand = self.results.multi_hand_landmarks[hand_no]
+            for id_lm, lm in enumerate(my_hand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                #                    print(id, cx, cy)
+                lm_list.append(id_lm, cx, cy)
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0.255), cv2.FILLED)
+        return lm_list
+
+
+def main():
+    p_time = 0
+    c_time = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
+
+    while True:
+        success, img = cap.read()
+        img = detector.find_hands(img)
+        lm_list = detector.find_position(img)
+
+        if len(lm_list) != 0:
+            print(lm_list[4])
+
+        c_time = time.time()
+        fps = 1 / (c_time - p_time)
+        p_time = c_time
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+    main()
